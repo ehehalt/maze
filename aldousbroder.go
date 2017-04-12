1f58ddc85d766cd3defda81933e2785c7b37f104
M. Ehehalt <mike42@hotmail.de>
2017-04-12 17:38:23 +0200

    Debug stuff removed (part 2)

--- aldousbroder.go
+++ aldousbroder.go
@@ -17,13 +17,7 @@ func AldousBroder(g Grid) *Grid {
 	for unvisited > 0 { // && step < 10 {
 		nbs := c.Neighbors()
 		rnd := rand.Intn(len(nbs))
-
-		// tmp := fmt.Sprintf("rand[0-%d]=%d", len(nbs), rnd)
-		// fmt.Print(tmp)
-
 		nb := nbs[rnd]
-
-		// fmt.Println(":", c.ToString(), " => ", nb.ToString())
 
 		if len(nb.Links()) == 0 {
 			c.Link(nb)
