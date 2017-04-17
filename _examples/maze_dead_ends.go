bf4f3a405e0aacd2a35bdd1aba09a7d8f15cbff6
M. Ehehalt <mike42@hotmail.de>
2017-04-17 13:36:23 +0200

    DeadEnd program enhanced (concurrent, 100 tries)

--- _examples/maze_dead_ends.go
+++ _examples/maze_dead_ends.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/ehehalt/maze"
 )
@@ -22,19 +23,36 @@ var (
 		maze.Wilson,
 		maze.HuntAndKill,
 	}
+
+	tries = 100
+	size  = 20
 )
 
 func main() {
 
+	fmt.Println("Comparing DeadEnds in different algorithms.")
+	fmt.Println("Tries per algorithm:", tries)
+	fmt.Println("Size of the grids:", size, "x", size)
+	fmt.Println()
+
+	var wg sync.WaitGroup
+	wg.Add(len(names))
+
 	for idx, _ := range names {
+		go func(name string, algorithm maze.Generator) {
+			defer wg.Done()
+			total_deadends := 0
 
-		name := names[idx]
-		algorithm := algorithms[idx]
+			for try := 0; try < tries; try++ {
+				source := maze.NewGrid(size, size)
+				destination := algorithm(*source)
 
-		source := maze.NewGrid(10, 10)
-		destination := algorithm(*source)
+				total_deadends += len(destination.DeadEnds())
+			}
 
-		fmt.Printf("%20s: DeadEnds = %d\n", name, len(destination.DeadEnds()))
+			fmt.Printf("%20s = %d\n", name, total_deadends/tries)
+		}(names[idx], algorithms[idx])
 	}
 
+	wg.Wait()
 }
