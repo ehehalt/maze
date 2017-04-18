ace70e6bd0760ef12c4a50e02b5daaf452d3a6a6
M. Ehehalt <mike42@hotmail.de>
2017-04-18 19:08:54 +0200

    maze_to_png with recursive backtracker

--- _examples/maze_to_png.go
+++ _examples/maze_to_png.go
@@ -9,7 +9,8 @@ func main() {
 	// g = maze.Sidewinder(*g)
 	// g = maze.AldousBroder(*g)
 	// g = maze.Wilson(*g)
-	g = maze.HuntAndKill(*g)
+	// g = maze.HuntAndKill(*g)
+	g = maze.RecursiveBacktracker(*g)
 
 	maze.GridToPng(g, "maze.png", true, true)
 }
